165a170a4e3a2ea100218b78f054eb2bd050dada
Abdirahman <abdirahman.diis@student.moringaschool.com>
2023-07-21 23:19:54 +0300

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
@@ -6,17 +6,36 @@ function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const [darkMode, setDarkMode] = useState(false)
+  const appClass = darkMode ? "App dark" : "App light"
 
+  //create a fn to handle dark mode toggle on click
+  function handleDarkModeToggle(){
+    setDarkMode(!darkMode)
+  }
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleDarkModeToggle}>Dark Mode
+        </button>
       </header>
-      <ShoppingList items={itemData} />
+      <ShoppingList items={itemData}/>
     </div>
-  );
+  )
+
+
+
+  // ==> doing this makes the dark mode toggle fn act weird when removing or adding items to cart 
+  // return (
+  //   <div onClick={() => setDarkMode(!darkMode)}className={appClass}>
+  //     <header>
+  //       <h2>Shopster</h2>
+  //       <button>{darkMode ? 'Light Mode' : 'Dark Mode'}</button>
+  //     </header>
+  //     <ShoppingList items={itemData} />
+  //   </div>
+  // );
 }
 
 export default App;
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState }from "react";
 
 function Item({ name, category }) {
+  //create state variable to track if item is in cart
+  const [inCart, setInCart] = useState(false);
+  //set class name for item based on whether its in the cart or not
+  const itemClass = inCart ? "in-cart" : "";
+
+  //create fn to handle add/remove from cart on button click
+  function handleAddRemoveClick(){
+    setInCart(!inCart)
+  }
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleAddRemoveClick} className="add">
+      {inCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
